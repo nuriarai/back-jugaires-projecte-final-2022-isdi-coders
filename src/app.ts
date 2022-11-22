bc922fb6a241e6aaba66bf8b351b1d18f6302b5b
Núria Ramoneda <nuriarai@gmail.com>
2022-11-22 10:54:55 +0100

    Remove cors by the moment

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,9 @@
 import express from "express";
 import morgan from "morgan";
-import cors from "cors";
 
 const app = express();
 
 app.disable("x-powered-by");
-app.use(cors());
 
 app.use(morgan("dev"));
 app.use(express.json());

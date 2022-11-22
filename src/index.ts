a1cc9f7276dae72222933b6108e448dca9c95661
Núria Ramoneda <nuriarai@gmail.com>
2022-11-22 12:43:25 +0100

    Create function database connection & add cors to app

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import "./loadEnvirontment.js";
 import environtment from "./loadEnvirontment.js";
-import startServer from "./server/index.js";
-
-import app from "./app.js";
 import debug from "debug";
+import app from "./app.js";
+import startServer from "./server/index.js";
+import connectDb from "./database/index.js";
 
 const { port, dbUrl } = environtment;
 
+await connectDb(dbUrl);
 await startServer(app, Number(port));
 debug(dbUrl);
